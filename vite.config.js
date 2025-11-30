28f376dd2234cd016b56a6fed3a2bdc78ee81cdd
Freed0m <meseroff@gmail.com>
2025-11-30 20:41:15 +0300

    att1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/freed0m52/react_kr', // замени на имя твоего репозитория
 })
